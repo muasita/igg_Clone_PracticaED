1987c252dbfe662eaeac5f7fe3104eb95a3f80a6
muasita <mua_isa@hotmail.com>
2019-06-02 12:32:26 +0200

    #3 fix Moto
    #3 fix Moto

--- Practica6/src/practica6/Moto.java
+++ Practica6/src/practica6/Moto.java
@@ -1,82 +1,43 @@
 package practica6;
 
 /**
- * Clase Coche
+ * Clase Moto
  * @author IGG
  * @version 2019
 */
 
-
-public class Coche {
-	String matricula= "";
-	String atrib= "";
-	String modelo= "";
-	String fabricante= "";
+public class Moto {
+	String matricula="";
+	int cilindrada=0;
 
 	
-/*
-  ---REFACTORIZACIÓN-- ELIMINAR CODIGO DUPLICADO
-
-	public Coche() {
+/* 
+---REFACTORIZACIÓN-- ELIMINAR CODIGO DUPLICADO
+	 
+	public Moto() {
 		matricula = "";
-		atrib = "";
-		modelo = "";
-		fabricante = "";
+		cilindrada = 0;
 	}
 
-	public Coche(String m, String c, String mo, String f) {
+	public Moto(String m, int c) {
 		matricula = m;
-		atrib = c;
-		modelo = mo;
-		fabricante = f;
+		cilindrada = c;
 	}
 */
 	
-	
-	public String metodo1() {
-		String resultado = "";
-		String texto = TEXTO_INICIAL + matricula + " ";
-		if (atrib == "Gasolina") {
-			double precio=1.337;
-			resultado = texto + metodoInfoPrecio(precio);
-		} 
-		else if (atrib == "Diesel") {
-			double precio=1.052;
-			resultado = texto + metodoInfoPrecio(precio);
-		} 
-		else if (atrib == "Híbrido") {
+	public String getMatricula() {
+		return matricula;
+	}
 
-			resultado = texto + metodoNoInfoPrecio();
-		} 
-		else {
-			resultado = texto + "no dispone de información";
-		}
-		return resultado;
+	public int getCilindrada() {
+		return cilindrada;
 	}
-	
-	//	  ---REFACTORIZACIÓN-- EXTRAER CONSTANTE
-	  
-	static final String TEXTO_INICIAL = "El coche con matricula";
-	
-    /**
-     * Metodo metodoInfoPrecio.
-     * devuelve información sobre el precio del carburante
-     * @precio indica el precio
-     * @return devuelve cadena de texto
-     */  
-	
-	public String metodoInfoPrecio(double precio) {
-		String info ="es un " + modelo + " y gasta" + precio +" euros por litro.";
-		return info;
+
+	public void setMatricula(String m) {
+		matricula = m;
 	}
-	
-    /**
-     * Metodo metodoNoInfoPrecio.
-     * devuelve información
-     * @return devuelve cadena de texto
-     */ 
-	public String metodoNoInfoPrecio() {
-		String info ="es un " + modelo + " y no necesita combustible";
-		return info;
+
+	public void setCilindrada(int c) {
+		cilindrada = c;
 	}
 }
